bd810fcb1ae2b6b2ad73b0e71b18007f99b67b5e
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-04-05 10:16:00 -0500

    message improvement in the relevant languages

--- Front-End/src/components/layout/NavbarInfo.jsx
+++ Front-End/src/components/layout/NavbarInfo.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
 
 function NavbarInfo({ role }) {
   const [time, setTime] = useState(new Date())
+  const { t } = useTranslation()
 
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 1000)
@@ -11,7 +13,7 @@ function NavbarInfo({ role }) {
   return (
     <div className="navbar-info">
       <span className="navbar-info-greeting">
-        Hola {role === "admin" ? "Administrador" : "Usuario"}
+        {t('nav.greeting')} {role === "admin" ? t('nav.admin') : t('nav.user')}
       </span>
       <span className="navbar-info-time">
         {time.toLocaleDateString()} {time.toLocaleTimeString()}
