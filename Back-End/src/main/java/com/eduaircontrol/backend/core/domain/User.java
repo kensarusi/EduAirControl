839853614e431b4922d5023f62ca048c99ccce6e
Jhon-Cortez <cortezvargasjhon01@gmail.com>
2026-04-09 22:38:40 -0500

    feat:User domain created

---
+++ Back-End/src/main/java/com/eduaircontrol/backend/core/domain/User.java
@@ -0,0 +1,26 @@
+package com.eduaircontrol.backend.core.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+    
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+}
