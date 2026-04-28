d859768efec7ff45dd935bdb6d874a67d8f62e0b
Jhon-Cortez <cortezvargasjhon01@gmail.com>
2026-04-28 16:02:29 -0500

    feat:show user email token

--- Web/backend/src/main/java/com/eduaircontrol/backend/application/controller/Test.java
+++ Web/backend/src/main/java/com/eduaircontrol/backend/application/controller/Test.java
@@ -1,14 +1,24 @@
 package com.eduaircontrol.backend.application.controller;
 
+import com.eduaircontrol.backend.application.dto.LoginRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class Test {
+    
+    private final LoginRequest  auth;
+    
     @GetMapping("/test")
     public String test(){
         return "Contenido";
+    }
+    @GetMapping("/me")
+    public String me(){
+        return auth.getEmail();
     }
 }
--- Web/backend/src/main/java/com/eduaircontrol/backend/config/SecurityConfig.java
+++ Web/backend/src/main/java/com/eduaircontrol/backend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springdoc.core.service.GenericParameterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,6 +22,8 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/test").hasRole("USER")
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers(
                     "/auth/**",
                     "/v3/api-docs/**",
