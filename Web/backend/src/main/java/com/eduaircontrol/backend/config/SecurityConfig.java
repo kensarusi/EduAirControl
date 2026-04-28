78c2b5ef7c153c83cec139396c2355ef9cd4b4d6
Jhon-Cortez <cortezvargasjhon01@gmail.com>
2026-04-28 14:33:16 -0500

    feat:Jwtd filter created

--- Web/backend/src/main/java/com/eduaircontrol/backend/config/SecurityConfig.java
+++ Web/backend/src/main/java/com/eduaircontrol/backend/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.eduaircontrol.backend.config;
 
+import com.eduaircontrol.backend.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.service.GenericParameterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
@@ -21,8 +28,8 @@ public class SecurityConfig {
                     "/swager-ui.html")
                     .permitAll()
                     .anyRequest().authenticated()
-            );
-            
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);//Antes de que valide el usuario ejecuta el filtro jwt
         
         return http.build();
     }
