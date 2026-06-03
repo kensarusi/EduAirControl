3ef5eccc9c4a874c7eea1898aa7747e879c41c8d
Jhon-Cortez <cortezvargasjhon@gmail.com>
2026-06-03 12:59:27 -0500

    feat:added configuration to google

--- Web/backend/src/main/java/com/eduaircontrol/backend/config/SecurityConfig.java
+++ Web/backend/src/main/java/com/eduaircontrol/backend/config/SecurityConfig.java
@@ -2,38 +2,62 @@ package com.eduaircontrol.backend.config;
 
 import com.eduaircontrol.backend.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.core.service.GenericParameterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
     
     private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/test").hasRole("USER")
-                .requestMatchers("/user/**").hasRole("USER")
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers(
-                    "/auth/**",
-                    "/v3/api-docs/**",
-                    "/swagger-ui/**",
-                    "/swager-ui.html")
-                    .permitAll()
-                    .anyRequest().authenticated()
-            )
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);//Antes de que valide el usuario ejecuta el filtro jwt
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         
-        return http.build();
+        return httpSecurity
+                .authorizeHttpRequests(request ->{
+                    request.requestMatchers(HttpMethod.GET,"/").permitAll();
+                    request.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // http
+        //     .csrf(csrf -> csrf.disable())
+        //     .authorizeHttpRequests(auth -> auth
+        //         .requestMatchers("/test").hasRole("USER")
+        //         .requestMatchers("/user/**").hasRole("USER")
+        //         .requestMatchers("/admin/**").hasRole("ADMIN")
+        //         .requestMatchers(
+        //             "/auth/**",
+        //             "/v3/api-docs/**",
+        //             "/swagger-ui/**",
+        //             "/swager-ui.html")
+        //             .permitAll()
+        //             .anyRequest().authenticated()
+        //     )
+        //     .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);//Antes de que valide el usuario ejecuta el filtro jwt
+        
+        // return http.build();
     }
 }
