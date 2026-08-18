4fc22d5caff7bf586797fe4497a1ae7b0643afc3
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-08-18 14:49:41 -0500

    feat: update landing page

--- Web/Front-End/src/modules/landing/components/Hero/Hero/Hero.jsx
+++ Web/Front-End/src/modules/landing/components/Hero/Hero/Hero.jsx
@@ -2,33 +2,19 @@ import "./Hero.css";
 
 import HeroContent from "../HeroContent/HeroContent";
 import HeroCards from "../HeroCards/HeroCards";
-import HeroDashboard from "../HeroDashboard/HeroDashboard";
 
 function Hero() {
   return (
     <section id="hero" className="hero">
-
-      {/* Luces del fondo */}
       <div className="hero-glow hero-glow-left"></div>
       <div className="hero-glow hero-glow-right"></div>
 
       <div className="hero-container">
-
-        <div className="hero-left">
-          <HeroContent />
-          <HeroCards />
-        </div>
-
-          <div className="dashboard-glow"></div>
-
-        <div className="hero-right">
-          <HeroDashboard />
-        </div>
-
+        <HeroContent />
+        <HeroCards />
       </div>
 
-        <div className="hero-transition"></div>
-
+      <div className="hero-transition"></div>
     </section>
   );
 }
